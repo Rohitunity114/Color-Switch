912d365248f970e4b7f097a4b3eda48ce3d72350
Rohit <rohitunity114@gmail.com>
2020-01-29 16:33:01 -0500

    Script,Scane

---
+++ Color Switch/Assets/Script/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float Speed = 100f;
+   
+    void Update()
+    {
+        transform.Rotate(0f, 0f, Speed * Time.deltaTime);
+    }
+}
---
+++ Color Switch/Assets/Script/player.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public string ColorMatch;
+    
+    public float MoveSpeed = 10f;
+
+    public Rigidbody2D rb;
+    public SpriteRenderer sp;
+
+    public Color Sky;
+    public Color Yellow;
+    public Color Blue;
+    public Color Pink;
+    void Start()
+    {
+        SwitchColor();
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector3.up * MoveSpeed;
+        }       
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.tag != ColorMatch)
+        {
+            Debug.Log("Game Over");
+        }    
+        if(coll.tag == "ColorChanger")
+        {
+            SwitchColor();
+            Destroy(coll.gameObject);
+            return;           
+        }
+        if(coll.tag == "Win")
+        {
+            Debug.Log("Game Win!!");
+            Destroy(coll.gameObject);
+            Destroy(gameObject);
+            return;
+        }
+    }
+   
+    private void SwitchColor()
+    {
+        float Number = Random.Range(0, 4);
+
+        switch(Number)
+        {
+            case 0:
+                {
+                    ColorMatch = "Sky";
+                    sp.color = Sky;
+                    break;
+                }
+            case 1:
+                {
+                    ColorMatch = "Yellow";
+                    sp.color = Yellow;
+                    break;
+                }
+            case 2:
+                {
+                    ColorMatch = "Blue";
+                    sp.color = Blue;
+                    break;
+                }
+            case 3:
+                {
+                    ColorMatch = "Pink";
+                    sp.color = Pink;
+                    break;
+                }
+        }
+    }
+
+}
