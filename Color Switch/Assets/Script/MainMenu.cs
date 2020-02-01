6504e03362349bd06a46259295ae8afa8aae32a7
Rohit <rohitunity114@gmail.com>
2020-02-01 09:10:59 -0500

    Add new

---
+++ Color Switch/Assets/Script/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public ScanFeder feder;
+    public void PlayGame(string gameMenuName)
+    {
+        feder.FadeTo(gameMenuName);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
