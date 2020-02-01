3501d6057e596ad9383405020c0c748d6cbb93ec
Rohit <rohitunity114@gmail.com>
2020-02-01 10:55:50 -0500

    Addlevel

--- Color Switch/Assets/Script/LevelSelect.cs
+++ Color Switch/Assets/Script/LevelSelect.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelect : MonoBehaviour
 {
     public ScanFeder fader;
-    // Start is called before the first frame update
+    public Button[] levelButtons;
+   
     void Start()
     {
-        
+        int levelReched = PlayerPrefs.GetInt("LevelReched", 1);
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if(i + 1 > levelReched)
+            {
+                levelButtons[i].interactable = false;
+            }
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
     public void Select(string LevelName)
     {
         fader.FadeTo(LevelName);
